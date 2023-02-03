a3776e8d836322ee1d51d006a822735e9301840c
Adult96 <djxor007@gmail.com>
2023-02-03 16:26:11 +0900

    feat: add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className='navBar'>
+      <span>My Todo List</span>
+      <span>React</span>
+    </div>
+  );
+}
